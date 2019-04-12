97d480b4fbb2a82a5a9cd0a7a9eb7db9f86fcf09
Thomas Gubien <thomas.gubien@outlook.fr>
2019-04-12 11:08:40 +0200

    Create storage implements

---
+++ src/main/groovy/store/MemoryProductStorage.groovy
@@ -0,0 +1,28 @@
+package store
+
+class MemoryProductStorage implements ProductStorage{
+    @Override
+    void save(Product p) {
+
+    }
+
+    @Override
+    void update(String id, Product p) {
+
+    }
+
+    @Override
+    Product getByID(String id) {
+        return null
+    }
+
+    @Override
+    void delete(String id) {
+
+    }
+
+    @Override
+    List<Product> all() {
+        return null
+    }
+}
---
+++ src/test/groovy/store/MemoryProductStorageTest.groovy
@@ -0,0 +1,33 @@
+package store
+
+class MemoryProductStorageTest extends GroovyTestCase {
+    void testSave() {
+    }
+
+    void testUpdate() {
+    }
+
+    void testGetByID() {
+    }
+
+    void testDelete() {
+    }
+
+    void testAll() {
+    }
+
+    void testInvokeMethod() {
+    }
+
+    void testGetProperty() {
+    }
+
+    void testSetProperty() {
+    }
+
+    void testGetMetaClass() {
+    }
+
+    void testSetMetaClass() {
+    }
+}
