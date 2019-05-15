f42e33bc8fc074f795e54289f9d3d0674b643103
Thomas Gubien <thomas.gubien@outlook.fr>
2019-05-15 13:54:20 +0200

    Update Product tests

--- src/test/groovy/store/ProductTest.groovy
+++ src/test/groovy/store/ProductTest.groovy
@@ -7,6 +7,8 @@ class ProductTest extends Specification {
 
         when:
         Product product = new Product(id: "1", name: "myProduct", description: "Product description", price: 10, idealTemperature: 20)
+        Product product2 = new Product(id: "2", name: "myProduct2")
+        Product product3 = new Product()
 
         then:
         product.id == "1"
@@ -14,6 +16,11 @@ class ProductTest extends Specification {
         product.description == "Product description"
         product.price == 10
         product.idealTemperature == 20
+
+        product2.id == "2"
+        product2.name == "myProduct2"
+
+        product3.id == null
     }
 
 
