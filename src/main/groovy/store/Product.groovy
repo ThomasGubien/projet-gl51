7730b74bfd66c4468b56f505c597beedd2ee243a
Thomas Gubien <thomas.gubien@outlook.fr>
2019-05-16 13:41:10 +0200

    Product tests update

--- src/main/groovy/store/Product.groovy
+++ src/main/groovy/store/Product.groovy
@@ -1,11 +1,11 @@
 package store
 
 class Product {
-    String id
-    String name
-    String description
-    double price
-    double idealTemperature
+    private String id
+    private String name
+    private String description
+    private double price
+    private double idealTemperature
 
     Product(String id, String name, String description, double price, double idealTemperature) {
         this.id = id
@@ -21,5 +21,49 @@ class Product {
     }
 
     Product() {
+    }
+
+    void finalized(){
+        null
+    }
+
+    void updatePrice(double price){
+        this.price = price
+    }
+
+    void update(Product p){
+        this.name = p.name
+        this.description = p.description
+        this.price = p.price
+        this.idealTemperature = p.idealTemperature
+    }
+
+    @Override
+    String toString(){
+        String string = name + ", price: " + price
+    }
+
+    Product getProduct(){
+        this
+    }
+
+    String getId(){
+        this.id
+    }
+
+    String getName(){
+        this.name
+    }
+
+    String getDescription(){
+        this.description
+    }
+
+    double getPrice(){
+        this.price
+    }
+
+    double getIdealTemperature(){
+        this.idealTemperature
     }
 }
--- src/test/groovy/store/ProductTest.groovy
+++ src/test/groovy/store/ProductTest.groovy
@@ -3,6 +3,10 @@ package store
 import spock.lang.Specification
 
 class ProductTest extends Specification {
+
+
+    Product p = new Product(id: "1", name: "myProduct", description: "Product description", price: 10, idealTemperature: 20)
+
     def "create product test"(){
 
         when:
@@ -21,6 +25,52 @@ class ProductTest extends Specification {
         product2.name == "myProduct2"
 
         product3.id == null
+    }
+
+    def "delete product test"(){
+
+        when:
+        p = null
+
+        then:
+        p == null
+    }
+
+    def "Update product test"(){
+        setup:
+        Product p2 = new Product(id: "id2", name: "product 2", description: "description 2", price: 40, idealTemperature: 5)
+
+        when:
+        p.updatePrice(30)
+        p2.update(p)
+
+        then:
+        p.price == 30
+        p2.name == p.name
+        p2.description == p.description
+        p2.price == p.price
+        p2.idealTemperature == p.idealTemperature
+    }
+
+    def "get string product test"(){
+        setup:
+        String s = ""
+
+        when:
+        s = p.toString()
+
+        then:
+        s == "myProduct, price: 10.0"
+    }
+
+    def "get product test"(){
+        when:
+        Product p2 = p.getProduct()
+        String s = p.getName()
+
+        then:
+        p2 == p
+        s == "myProduct"
     }
 
 
